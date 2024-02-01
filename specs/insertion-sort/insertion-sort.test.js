2af37413c89ff155c37e73aeae7e30b8e5d37878
Gayle Martin <hello.gmartin@gmail.com>
2024-02-01 09:05:44 -0600

    Complete insertion sort challenge

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -13,13 +13,32 @@
   And you put xdescribe instead of describe if you want to suspend running the unit tests.  
 */
 
+// Insertion sort is better than bubble sort
+
 function insertionSort(nums) {
-  // code goes here
+  // Loop through the array beginning at index 1
+  for (let i = 1; i < nums.length; i++) {
+    // declare number to insert, this stores it because we will overwrite it before we insert it
+    let numberToInsert = nums[i];
+    let j;
+    // Loop through again beginning at index i - 1
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      // move the numbers to the right until we find where to insert
+      nums[j + 1] = nums[j];
+    }
+    // do the insertion
+    nums[j + 1] = numberToInsert;
+  }
+  return nums;
 }
+
+const arr = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+const result = insertionSort(arr);
+console.log(result);
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test.skip('insertion sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
