3c276561c85e630850be07202e561a6a5ec40e07
Gayle Martin <hello.gmartin@gmail.com>
2024-02-01 09:10:43 -0600

    Complete array list challenge

--- specs/arrays/arraylist.test.js
+++ specs/arrays/arraylist.test.js
@@ -18,61 +18,100 @@
 */
 
 class ArrayList {
-  // code goes here
+  constructor() {
+    // instantiate variables
+    // length = How many elements in the array
+    this.data = {};
+    this.length = 0;
+  }
+  push(value) {
+    // accepts a value and adds to the end of the list
+    this.data[this.length] = value;
+    this.length++;
+  }
+  pop() {
+    // removes the last value in the list and returns it
+    const popped = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return popped;
+  }
+  get(index) {
+    // accepts an index and returns the value at that position
+    return this.data[index];
+  }
+  delete(index) {
+    // accepts an index, removes value from list, collapses, and returns removed value
+    const result = this.data[index];
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.length--;
+    return result;
+  }
 }
+
+const newObj = new ArrayList;
+console.log('newObj: ', newObj);
+
+newObj.push(1)
+newObj.push(5);
+newObj.push(10);
+console.log(newObj);
+console.log(newObj.pop())
+
+
 
 // unit tests
 // do not modify the below code
-describe.skip("ArrayList", function () {
-  const range = (length) =>
-    Array.apply(null, { length: length }).map(Number.call, Number);
-  const abcRange = (length) =>
-    range(length).map((num) => String.fromCharCode(97 + num));
+describe('ArrayList', function () {
+  const range = length => Array.apply(null, { length: length }).map(Number.call, Number);
+  const abcRange = length => range(length).map(num => String.fromCharCode(97 + num));
   let list;
 
   beforeEach(() => {
     list = new ArrayList();
   });
 
-  test("constructor", () => {
+  test('constructor', () => {
     expect(list).toEqual(expect.any(ArrayList));
   });
 
-  test("push", () => {
-    abcRange(26).map((character) => list.push(character));
+  test('push', () => {
+    abcRange(26).map(character => list.push(character));
     expect(list.length).toEqual(26);
   });
 
-  test("pop", () => {
-    abcRange(13).map((character) => list.push(character));
+  test('pop', () => {
+    abcRange(13).map(character => list.push(character));
     expect(list.length).toEqual(13);
     range(10).map(() => list.pop());
     expect(list.length).toEqual(3);
-    expect(list.pop()).toEqual("c");
+    expect(list.pop()).toEqual('c');
   });
 
-  test("get", () => {
-    list.push("first");
-    expect(list.get(0)).toEqual("first");
-    list.push("second");
-    expect(list.get(1)).toEqual("second");
-    expect(list.get(0)).toEqual("first");
-    abcRange(26).map((character) => list.push(character));
-    expect(list.get(27)).toEqual("z");
-    expect(list.get(0)).toEqual("first");
-    expect(list.get(9)).toEqual("h");
+  test('get', () => {
+    list.push('first');
+    expect(list.get(0)).toEqual('first');
+    list.push('second');
+    expect(list.get(1)).toEqual('second');
+    expect(list.get(0)).toEqual('first');
+    abcRange(26).map(character => list.push(character));
+    expect(list.get(27)).toEqual('z');
+    expect(list.get(0)).toEqual('first');
+    expect(list.get(9)).toEqual('h');
     list.pop();
-    expect(list.get(list.length - 1)).toEqual("y");
+    expect(list.get(list.length - 1)).toEqual('y');
   });
 
-  test("delete", () => {
-    abcRange(26).map((character) => list.push(character));
+  test('delete', () => {
+    abcRange(26).map(character => list.push(character));
     list.delete(13);
     expect(list.length).toEqual(25);
-    expect(list.get(12)).toEqual("m");
-    expect(list.get(13)).toEqual("o");
+    expect(list.get(12)).toEqual('m');
+    expect(list.get(13)).toEqual('o');
     list.delete(0);
     expect(list.length).toEqual(24);
-    expect(list.get(0)).toEqual("b");
+    expect(list.get(0)).toEqual('b');
   });
 });
